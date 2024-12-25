a272c65cf246bbed7b12243870e2142e7e2a058e
dajamplay <ggyoug@gmail.com>
2024-12-25 16:44:02 +0300

    add animation for player

--- src/components/player/Player.jsx
+++ src/components/player/Player.jsx
@@ -9,6 +9,7 @@ import {
 } from "@fortawesome/free-solid-svg-icons";
 import {faPause} from "@fortawesome/free-solid-svg-icons/faPause";
 import {Link} from "react-router-dom";
+import TrackImage from "@/components/track/TrackImage.jsx";
 
 const Player = () => {
 
@@ -16,6 +17,14 @@ const Player = () => {
 
     const inputHandler = (e) => {
         seekTo(e.target.value);
+    }
+
+    const playPause = () => {
+        if (isPlaying) {
+            pause();
+        } else {
+            play();
+        }
     }
 
     return (
@@ -33,14 +42,14 @@ const Player = () => {
             </div>
             {Object.keys(currentTrack).length > 0 &&
                 <div className={styles.control_container}>
-                    <Link to={'/queue'}>
-                        <img
+                    <div onClick={playPause}>
+                        <TrackImage
+                            isPlaying={isPlaying}
+                            isActive={true}
                             src={import.meta.env.VITE_APP_BASE_URL + currentTrack.artwork}
                             alt={currentTrack.title}
-                            width='50'
-                            className={styles.control_container_image}
                         />
-                    </Link>
+                    </div>
                     <div className={styles.control_container_description}>
                         <div className={styles.control_container_description_title}>
                             {currentTrack.title}
--- src/components/track/TrackImage.jsx
+++ src/components/track/TrackImage.jsx
@@ -3,7 +3,7 @@ import gifImg from "@/assets/img/audio-eq.gif";
 
 const TrackImage = ({src, alt, isActive = false, isPlaying = false}) => {
     return(
-        <>
+        <div className={styles.image_container}>
             <img
                 src={src}
                 alt={alt}
@@ -16,7 +16,7 @@ const TrackImage = ({src, alt, isActive = false, isPlaying = false}) => {
                     className={styles.track_image_eq}
                 />
             }
-        </>
+        </div>
     );
 }
 
